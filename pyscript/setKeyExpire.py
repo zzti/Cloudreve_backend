7a5337a32915438a4d7aa009657f53e6126b50aa
张斌 <bin.z@msn.cn>
2024-05-29 16:06:18 +0800

    python redis库使用官方推荐的StrictRedis

--- pyscript/setKeyExpire.py
+++ pyscript/setKeyExpire.py
@@ -1,16 +1,16 @@
-from redis import Redis
+from redis import StrictRedis
 def setExpiredKeys():
     try:
         if redis_pass == 'none':
-            redisclient = Redis(host=redis_host, port=redis_port,db=1)
+            redisclient = StrictRedis(host=redis_host, port=redis_port,db=1)
         else:
-            redisclient = Redis(host=redis_host, port=redis_port, password=redis_pass, db=1)
+            redisclient = StrictRedis(host=redis_host, port=redis_port, password=redis_pass, db=1)
         for key in redisclient.scan_iter(match="cd_session_*",count=10):
             print(key)
             #执行redis命令
             #keyttl = redisclient.execute_command('ttl', key)
             #if keyttl == -1: # 此处扫到key，可以进行导出处理或者执行命令
-            redisclient.expire(key, 7200)
+            #redisclient.expire(key, 7200)
     except Exception as e:
         raise e
 if __name__ == '__main__':
